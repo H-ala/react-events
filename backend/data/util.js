3eb2e79c14c55087890c1dbf9d9425a8dbe64e4a
HoseinAlaee <hos.ala81@gmail.com>
2025-10-22 22:50:40 +0330

    modify read and write func in node js

--- backend/data/util.js
+++ backend/data/util.js
@@ -1,12 +1,30 @@
+// const fs = require('node:fs/promises');
+
+// async function readData() {
+//   const data = await fs.readFile('events.json', 'utf8');
+//   return JSON.parse(data);
+// }
+
+// async function writeData(data) {
+//   await fs.writeFile('events.json', JSON.stringify(data));
+// }
+
+// exports.readData = readData;
+// exports.writeData = writeData;
+
 const fs = require('node:fs/promises');
+const path = require('path');
+
+// مسیر فایل نسبت به فولدر فعلی این فایل
+const eventsPath = path.join(__dirname, 'events.json');
 
 async function readData() {
-  const data = await fs.readFile('events.json', 'utf8');
+  const data = await fs.readFile(eventsPath, 'utf8');
   return JSON.parse(data);
 }
 
 async function writeData(data) {
-  await fs.writeFile('events.json', JSON.stringify(data));
+  await fs.writeFile(eventsPath, JSON.stringify(data));
 }
 
 exports.readData = readData;
